bd4f636aaf315a40773096353914305c70e57c3a
AlexeySanchez <a16320150408.1@gmail.com>
2025-09-17 19:48:08 +0500

    Hello world

---
+++ Hello_world/Hello_world/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello_world
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Hello, world");
+            Console.ResetColor();
+
+            Console.ReadKey();
+        }
+    }
+}
