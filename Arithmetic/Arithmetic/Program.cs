2d2baaa2530004b7e1a6da3f3d367352a0c4f248
AlexeySanchez <a16320150408.1@gmail.com>
2025-10-01 20:01:42 +0500

    Вычисления

---
+++ Arithmetic/Arithmetic/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arithmetic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter 1st number");
+            int m=int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter 2nd number");
+            int n=int.Parse(Console.ReadLine());
+
+            int remainder = m % n;
+
+            remainder *= 10;
+            int tenth = remainder / n;
+            remainder %= n;
+
+            remainder *= 10;
+            int hundredth = remainder / n;
+            remainder %= n;
+
+            remainder *= 10;
+            int thousandth = remainder / n;
+
+            Console.WriteLine($"Десятые: {tenth}");
+            Console.WriteLine($"Сотые: {hundredth}");
+            Console.WriteLine($"Тысячные: {thousandth}");
+        }
+    }
+}
---
+++ Methods/Methods/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = InputInteger("Enter 1st coordinate");
+            var y = InputInteger("Enter 2nd coordinate");
+
+            Console.WriteLine("Point ("+x+","+y+") ");
+        }
+        static int InputInteger(string message)
+        {
+
+            Console.WriteLine(message);
+            return int.Parse(Console.ReadLine());
+        }
+    }
+}
