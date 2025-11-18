0d2a417da1d5c1bb1554dd9180bc7a701d93d693
AlexeySanchez <a16320150408.1@gmail.com>
2025-11-18 23:50:18 +0500

    10-2 final

--- Task-10-2/Task-10-2/Program.cs
+++ Task-10-2/Task-10-2/Program.cs
@@ -10,6 +10,18 @@ namespace Task_10_2
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Введите кол-во n");
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите a0");
+            int prev = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Введите a{i}");
+                int current = int.Parse(Console.ReadLine());
+                Console.Write($"a{i-1} + a{i} = {prev + current} \n"); 
+                prev = current;
+            }
         }
     }
 }
