6c6779435e4eed934025b5b0ef060df37f8fc699
AlexeySanchez <a16320150408.1@gmail.com>
2025-11-18 23:16:04 +0500

    10-1 готово

---
+++ Task-10-1/Task-10-1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_10_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int k = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= 9; i++)
+            {
+                Console.WriteLine($"{k} * {i} = {k * i}");
+            }
+        }
+    }
+}
