b9e6459093a9ed449b44d3cf1b46b2de9cb3b10f
AlexeySanchez <a16320150408.1@gmail.com>
2025-11-19 00:08:02 +0500

    10-4 in question

---
+++ Task-10-4/Task-10-4/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_10_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ///К сожалению, я не знаю, как решить данную задачу
+        }
+    }
+}
