c70610a5520fa9ebe8aa026fed6977089958fca7
AlexeySanchez <a16320150408.1@gmail.com>
2025-11-19 01:06:32 +0500

    10-5 final

---
+++ Task-10-5/Task-10-5/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_10_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Увеличение площади посева");
+            double x = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Увеличение сбора урожая");
+            double y = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Кол-во тонн");
+            double n = double.Parse(Console.ReadLine());
+
+            double area = 10;
+            double barley = 2; ///2 тонны = 20 центнеров 
+            double totalHarvest = 0;
+            int year = 1;
+
+            while (true)
+            {
+                totalHarvest += area * barley;
+                Console.WriteLine($"Тонн за все года: {totalHarvest:F2}");
+
+                if (totalHarvest > n)
+                {
+                    area *= 1 + x / 100;
+                    Console.WriteLine($"Площадь посева в га = {area:F2}");
+                    barley *= 1 + y / 100;
+                    Console.WriteLine($"Тонн ячменя за этот год = {barley * area:F2}");
+
+                    Console.WriteLine($"Год {year}");
+                    break;
+                }
+                else
+                {
+                    area *= 1 + x / 100;
+                    Console.WriteLine($"Площадь посева в га = {area:F2}");
+                    barley *= 1 + y / 100;
+                    Console.WriteLine($"Тонн ячменя за этот год = {barley*area:F2}");
+
+                    Console.WriteLine($"Год {year}");
+                }
+                year++;
+            }
+        }
+    }
+}
---
+++ Task-10-5/Task-10-5/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Task-10-5")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Task-10-5")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("8361e294-8f36-472d-8138-529516425463")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
