2ce10555deea0bd7c5996dc9435bac4fd5fda722
AlexeySanchez <a16320150408.1@gmail.com>
2025-11-19 00:03:31 +0500

    10-3 final

---
+++ Task-10-3/Task-10-3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_10_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число n");
+            double n = int.Parse(Console.ReadLine());
+            double m = 0;
+
+            while (true)
+            {
+                double result = Math.Pow(m,3) + m + 1;
+             
+                if (result > n)
+                {
+                    Console.WriteLine($"{m}^3 + {m} + 1 = {result} - наименьшее превосходящее");
+                    break;
+                }
+                else Console.WriteLine($"{m}^3 + {m} + 1 = {result}");
+                m++;
+            }
+        }
+    }
+}
---
+++ Task-10-3/Task-10-3/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Task-10-3")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Task-10-3")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("14dd9442-cab4-47a2-a42c-c26a84694db1")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
