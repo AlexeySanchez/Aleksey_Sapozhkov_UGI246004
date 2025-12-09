7077ebbc884f19ed713d2aad7785cc9570c85465
AlexeySanchez <a16320150408.1@gmail.com>
2025-12-09 17:48:24 +0500

    Matrix start

---
+++ Task-12/Task-12/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int m = 0;
+            int n = 0;
+            while (true)
+            {
+                Console.WriteLine("Enter two natural numbers separated by a space");
+                Console.WriteLine("Enter - refusal");
+
+                var input = Console.ReadLine();
+
+                if (input == string.Empty)
+                {
+                    return;
+                }
+                var strings = input.Split();
+
+                if (strings.Length == 2 && int.TryParse(strings[0], out m) && int.TryParse(strings[1], out n) && 5 <= m && m <= 20 && 5 <= n && n <= 20)
+                {
+                    break;
+                }
+
+                else
+                {
+                    Console.WriteLine("Error");
+                    continue;
+                }
+            }
+            var matrix = new int[m, n];
+            var rnd = new Random();
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                    matrix[i, j] = rnd.Next(0, 100);
+
+            PrintMatrix(matrix);
+        }
+        static void PrintMatrix(int[,] matrix)
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                    Console.Write($"{matrix[i, j],2} ");
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
